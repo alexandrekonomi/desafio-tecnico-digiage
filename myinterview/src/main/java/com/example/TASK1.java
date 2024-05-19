372f9e6887ff02a99b353a1f2155371ba8b5b2f4
Alexandre Konomi <alexandrekonomi1234@gmail.com>
2024-05-18 22:55:03 -0300

    feat: add task1 class method isPalindrome

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -1,18 +1,44 @@
 package com.example;
+
 import java.util.Scanner;
+
 /**
- * 
- *
  * Task here is to implement a function that says if a given string is
  * palindrome.
- * 
- * 
- * 
+ * <p>
+ * <p>
+ * <p>
  * Definition=> A palindrome is a word, phrase, number, or other sequence of
  * characters which reads the same backward as forward, such as madam or
  * racecar.
  */
 public class TASK1 {
-     
- 
+
+    public static boolean isPalindrome(String string) {
+        int left = 0;
+        int right = string.length() - 1;
+
+        while (left < right) {
+            if (string.charAt(left) != string.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome. ");
+        } else {
+            System.out.println("The string is not a palindrome. ");
+        }
+
+        scanner.close();
+    }
 }
