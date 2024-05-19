f15b8183e6143f7337ebc77b72e1d9b8978343b6
Alexandre Konomi <alexandrekonomi1234@gmail.com>
2024-05-18 22:59:19 -0300

    feat: add task3 class logic for create list and add aleatory number and print distinct items

--- myinterview/src/main/java/com/example/TASK3.java
+++ myinterview/src/main/java/com/example/TASK3.java
@@ -1,14 +1,27 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Write a list and add an aleatory number of Strings. In the end, print out how
  * many distinct itens exists on the list.
- *
  */
 public class TASK3 {
 
-  
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        Random random = new Random();
+
+        int numberOfElements = random.nextInt(20) + 1;
+        for (int i = 0; i < numberOfElements; i++) {
+            list.add("String" + random.nextInt(10));
+        }
+
+        System.out.println("List: " + list);
+
+        Set<String> distinctItems = new HashSet<String>(list);
+
+        System.out.println("Number of distinct items: " + distinctItems.size());
+    }
+
 }
